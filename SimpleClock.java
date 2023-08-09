b10af08a6f698b56b08abb6cad17ba5463741c82
Christine <christine@zipcodersMBP13.localdomain>
2023-08-09 10:51:22 -0400

    i finally finished this! only took some googling and finagling

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,22 +2,31 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
 
 
 public class SimpleClock extends JFrame implements Runnable{
-    //define a variable that controls the thread 1
+    //define a variable that controls the threads
     static boolean clockSet = false;
+    boolean militaryTime = false;
+    boolean timeZoneGMT = false;
 
-    Calendar calendar;
     SimpleDateFormat timeFormat;
+    SimpleDateFormat militaryTimeFormat;
     SimpleDateFormat dayFormat;
     SimpleDateFormat dateFormat;
 
     JLabel timeLabel;
     JLabel dayLabel;
     JLabel dateLabel;
+    JButton hourFormatButton;
+    JButton timezoneButton;
     String time;
     String day;
     String date;
@@ -30,23 +39,34 @@ public class SimpleClock extends JFrame implements Runnable{
         this.setResizable(false);
 
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        militaryTimeFormat = new SimpleDateFormat("HH:mm:ss");
         dayFormat = new SimpleDateFormat("EEEE");
         dateFormat = new SimpleDateFormat("dd MMMMM, yyyy");
+
         timeLabel = new JLabel();
         timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 59));
         timeLabel.setBackground(Color.BLACK);
         timeLabel.setForeground(Color.WHITE);
         timeLabel.setOpaque(true);
+
         dayLabel = new JLabel();
         dayLabel.setFont(new Font("Ink Free", Font.BOLD, 34));
 
         dateLabel = new JLabel();
         dateLabel.setFont(new Font("Ink Free", Font.BOLD, 30));
 
+        hourFormatButton = new JButton("12 / 24 hr format");
+        hourFormatButton.addActionListener(new HourFormat());
+
+        timezoneButton = new JButton("Local / GMT");
+        timezoneButton.addActionListener(new TimeZoneFormat());
 
         this.add(timeLabel);
         this.add(dayLabel);
         this.add(dateLabel);
+        this.add(hourFormatButton);
+        this.add(timezoneButton);
+        this.setSize(390, 230);
         this.setVisible(true);
 
 //        setTimer();
@@ -71,16 +91,47 @@ public class SimpleClock extends JFrame implements Runnable{
         }
     }
 
+    class HourFormat implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            militaryTime = !militaryTime;
+        }
+    }
+    class TimeZoneFormat implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            timeZoneGMT = !timeZoneGMT;
+            if(timeZoneGMT){
+                timeFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+                militaryTimeFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+            }
+            else{
+                timeFormat.setTimeZone(TimeZone.getDefault());
+                militaryTimeFormat.setTimeZone(TimeZone.getDefault());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         SimpleClock sc = new SimpleClock();
         Thread thread = new Thread(sc);
         thread.start();
     }
 
+    private String getCurrentTime(){
+        Date currentDate = Calendar.getInstance().getTime();
+        if(!militaryTime) {
+            return timeFormat.format(currentDate);
+        }
+        else{
+            return militaryTimeFormat.format(currentDate);
+        }
+    }
+
     @Override
     public void run() {
         while(!clockSet) {
-            time = timeFormat.format(Calendar.getInstance().getTime());
+            time = getCurrentTime();
             timeLabel.setText(time);
 
             day = dayFormat.format(Calendar.getInstance().getTime());
